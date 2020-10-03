a90aab5b4713115565139867161eb4a09384b177
Esteban Provoste <esteprov@gmail.com>
2020-10-03 17:35:38 -0300

    First commit

---
+++ src/build/precache-manifest.fc15f1ee564d32fe234a34297fe60da3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca510a78fcbd38b1acf6e0d9a04ee27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5fc6884a4bd683d123f",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "42dcac51a0a1225718b8",
+    "url": "/static/js/2.50dc2fc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50dc2fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5fc6884a4bd683d123f",
+    "url": "/static/js/main.d0be4f13.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
